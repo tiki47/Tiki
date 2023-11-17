ab27e64a67499b1add42d40405e3724eef5ba5dc
Joshua <jdcampbell4711@gmail.com>
2023-11-17 07:16:36 -0800

    Initial commit

---
+++ NetBeansProjects/HelloWorld/src/main/java/com/codewithtiki/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.codewithtiki.helloworld;
+
+/**
+ *
+ * @author joshu
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+    }
+}
